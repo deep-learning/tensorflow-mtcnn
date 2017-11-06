86e3393790741d17d48a3e167c312f045af10673
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2017-11-06 16:38:46 +0800

    try facenet

--- cpp/standalone/facenet_tf.cpp
+++ cpp/standalone/facenet_tf.cpp
@@ -7,9 +7,10 @@ const int scale_w = 160;
 
 static void dummy_deallocator(void *data, size_t len, void *arg) {}
 
-string input_layer = "input:0";
-string phase_train_layer = "phase_train:0";
-string output_layer = "embeddings:0";
+string input_layer = "input";
+string phase_train_layer = "phase_train";
+string output_layer = "embeddings";
+
 
 static int load_file2(const std::string &fname, std::vector<char> &buf) {
     std::ifstream fs(fname, std::ios::binary | std::ios::in);
@@ -68,14 +69,41 @@ TF_Session *load_graph2(const char *frozen_fname, TF_Graph **p_graph) {
     return session;
 }
 
+void dump_mat(Mat mat) {
+    std::cout << "(" << mat.cols << ", " << mat.rows << ", " << mat.channels() << ")" << endl;
+}
+
+static void BoolDeallocator(void *data, size_t, void *arg) {
+    delete[] static_cast<bool *>(data);
+}
+
+static TF_Tensor *boolTensor(bool v) {
+    const int num_bytes = sizeof(bool);
+    cout << "num_bytes=" << num_bytes << endl;
+    bool *values = new bool[1];
+    values[0] = v;
+    return TF_NewTensor(TF_BOOL, nullptr, 0, values, num_bytes, &BoolDeallocator, nullptr);
+}
+
+float cosine_similarity(const float * const A, const float * const B, unsigned int Vector_Length) {
+    float dot = 0.0f, denom_a = 0.0f, denom_b = 0.0f;
+    for (unsigned int i = 0u; i < Vector_Length; ++i) {
+        dot += A[i] * B[i];
+        denom_a += A[i] * A[i];
+        denom_b += B[i] * B[i];
+    }
+    return dot / (sqrt(denom_a) * sqrt(denom_b));
+}
+
+
 int main() {
+    printf("Hello from TensorFlow C library version %s\n", TF_Version());
     string model_fname = "/home/zhenglai/models/facenet_20170512-110547.pb";
     string test_img_fname = "/home/zhenglai/repo/tensorflow-mtcnn/cpp/standalone/id0.jpg";
-//    string test_img_fname = "/tmp/test.png";
-    string feature_vector_fname = "./test.csv";
-    TF_Session *sess;
-    TF_Graph *graph;
-    cv::Mat img_mat = cv::imread(test_img_fname.c_str(), IMREAD_COLOR);
+//    cv::Mat img_mat = cv::imread(test_img_fname.c_str(), IMREAD_COLOR);
+
+    Mat img_mat = Mat(160, 160, CV_8UC3);
+    randu(img_mat, Scalar::all(0), Scalar::all(255));
 
     if (!img_mat.data) {
         std::cerr << "failed to read image file: " << test_img_fname << std::endl;
@@ -87,20 +115,31 @@ int main() {
 
 
     cv::resize(img_mat, img_mat, cv::Size(scale_w, scale_h), 0, 0);
+    dump_mat(img_mat);
     img_mat.convertTo(img_mat, CV_32FC3);
-
+    cout << img_mat.at<float>(10, 10) << endl;
+    dump_mat(img_mat);
 
     /* tensorflow related*/
 
+    TF_Session *sess = nullptr;
+    TF_Graph *graph = nullptr;
     sess = load_graph2(model_fname.c_str(), &graph);
     TF_Status *s = TF_NewStatus();
 
-    std::vector<TF_Input> input_names;
+    assert(sess != nullptr);
+    assert(graph != nullptr);
+
+    std::vector<TF_Output> input_names;
     std::vector<TF_Tensor *> input_values;
 
     TF_Operation *input_name = TF_GraphOperationByName(graph, input_layer.c_str());
     TF_Operation *phase_train = TF_GraphOperationByName(graph, phase_train_layer.c_str());
 
+    assert(input_name != nullptr);
+    assert(phase_train != nullptr);
+
+    input_names.push_back({phase_train, 0});
     input_names.push_back({input_name, 0});
 
     const int64_t dim[4] = {1, scale_h, scale_w, 3};
@@ -112,14 +151,11 @@ int main() {
                                            sizeof(float) * scale_w * scale_h * 3,
                                            dummy_deallocator,
                                            nullptr);
-    TF_Tensor *phase_train_tensor = TF_NewTensor(TF_BOOL,
-                                                 new int64_t[]{1},
-                                                 1,
-                                                 img_mat.ptr(),
-                                                 sizeof(float) ,
-                                                 dummy_deallocator,
-                                                 nullptr);
 
+
+    TF_Tensor *phase_train_tensor = boolTensor(false);
+
+    input_values.push_back(phase_train_tensor);
     input_values.push_back(input_tensor);
 
     std::vector<TF_Output> output_names;
@@ -140,12 +176,18 @@ int main() {
                   nullptr,
                   s);
 
-
-    cout << "tf code" << TF_GetCode(s) << endl;
-//    assert(TF_GetCode(s) == TF_OK);
+    if (TF_GetCode(s) != TF_OK) {
+        cerr << TF_Message(s) << endl;
+    }
 
     const float *fvector = (const float *) TF_TensorData(output_values[0]);
-    cout << *fvector << endl;
+
+//    for (size_t i = 0; i < 128; ++i) {
+//        cout << fvector[i] << ',';
+//    }
+
+    cout << cosine_similarity(fvector, fvector, 128) << endl;
+
 
     TF_CloseSession(sess, s);
     TF_DeleteSession(sess, s);
